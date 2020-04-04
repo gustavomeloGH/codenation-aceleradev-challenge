0fc5edf33dea1e5fca642d4f68210d94486b0ab2
Gustavo Melo <gustavomelo.contato@gmail.com>
2020-04-04 01:10:33 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     codenation: {
         baseUrl: 'https://api.codenation.dev/v1/challenge/dev-ps',
-        token: 'b79e97c9eb224b17be833d925a6421dd3102778d'
+        token: 'seu_token_aqui'
     }
 }
 
